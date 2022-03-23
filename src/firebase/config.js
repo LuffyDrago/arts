610d3bd68d033b7023684040ecba2d64e301ff96
LuffyDrago <rigelvicky@gmail.com>
2022-03-23 15:31:40 +0300

    Firebase Google Sign in

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+// firebase config
+export const firebaseConfig = {
+  apiKey: "AIzaSyC8qGHTJ7Wbp0rbAHDUspCRIcz4cULGy70",
+
+  authDomain: "ecommerce-website-f75a1.firebaseapp.com",
+
+  projectId: "ecommerce-website-f75a1",
+
+  storageBucket: "ecommerce-website-f75a1.appspot.com",
+
+  messagingSenderId: "271093187001",
+
+  appId: "1:271093187001:web:aa73ca5a54d02e1ca67668",
+};
