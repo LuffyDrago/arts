2f79e928ad2c81fe74141c6ec98876cbfeab40d9
LuffyDrago <rigelvicky@gmail.com>
2022-03-24 14:29:41 +0300

    Firebase Email Registration and Login

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,19 +1,52 @@
 import React, { Component } from "react";
 // import "./styles.scss";
 import Buttons from "./../forms/Button";
-import { signInWithGoogle } from "./../../firebase/utils";
+import Button from "./../forms/Button";
+import FormInput from "../forms/FormInput";
+import { signInWithGoogle, auth } from "./../../firebase/utils";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 // const SignIn = (props) => {
 class SignIn extends Component {
-  // handleSubmit = async (e) => {
-  //   e.preventDefault();
-  // };
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
 
   handleSubmit = async (e) => {
     e.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        ...initialState,
+      });
+    } catch (err) {
+      //console.log(err)
+    }
   };
 
   render() {
+    const { email, password } = this.state;
+
     return (
       <div className="signin">
         <div className="wrap">
@@ -21,6 +54,26 @@ class SignIn extends Component {
 
           <div className="formWrap">
             <form onSubmit={this.handleSubmit}>
+              <FormInput
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                onChange={this.handleChange}
+                // handleChange={e => setEmail(e.target.value)}
+              />
+
+              <FormInput
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Password"
+                onChange={this.handleChange}
+                //   handleChange={e => setPassword(e.target.value)}
+              />
+              <Button type="submit">Login </Button>
+              <br />
+              <br />
               <div className="socialSignin">
                 <div className="row">
                   <Buttons onClick={signInWithGoogle}>
--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-//import Signup from './../../components/Signup';
-// import "./styles.scss";
+import Signup from "./../../components/Signup";
+//import "./styles.scss";
 
 const Registration = (props) => {
   return (
     <div className="">
-      <h1>hello</h1>
-      {/* <Signup /> */}
+      <Signup />
     </div>
   );
 };
